55915b6dc5989cdcde72a642ca564fc5c607bfe3
Katorly <i@katorly.com>
2024-11-22 16:27:41 +0000

    feat: add cloudflared install script

---
+++ debian/cloudflared/README.md
@@ -0,0 +1,17 @@
+## Install Docker
+Install cloudflared.
+
+### Features
+- Install cloudflared
+- Register cloudflared as a systemd service
+
+### Usage
+```bash
+bash <(curl -fsSL https://raw.githubusercontent.com/katorly/scripts/main/debian/cloudflared/install.sh)
+```
+or
+```bash
+bash <(wget -qO- https://raw.githubusercontent.com/katorly/scripts/main/debian/cloudflared/install.sh)
+```
+
+You'll have to get the token from Cloudflare One (Cloudflare Zero Trust) -> Networks -> Tunnels -> Create a tunnel
---
+++ debian/cloudflared/install.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+clear
+echo -e "====== Cloudflared install script - by katorly ======="
+
+read -p "Enter your cloudflared tunnel token: " TOKEN
+
+echo -e "\n\nInstalling cloudflared..."
+sudo mkdir -p --mode=0755 /usr/share/keyrings
+curl -fsSL https://pkg.cloudflare.com/cloudflare-main.gpg | sudo tee /usr/share/keyrings/cloudflare-main.gpg >/dev/null
+echo 'deb [signed-by=/usr/share/keyrings/cloudflare-main.gpg] https://pkg.cloudflare.com/cloudflared jammy main' | sudo tee /etc/apt/sources.list.d/cloudflared.list
+sudo apt-get update && sudo apt-get install cloudflared
+
+echo -e "\n\nRegistering cloudflared as a systemd service..."
+sudo tee /etc/systemd/system/cloudflared.service > /dev/null <<EOF
+[Unit]
+Description=Cloudflare Zero Trust Tunnel
+
+[Service]
+Type=simple
+ExecStart=cloudflared tunnel --edge-ip-version auto --protocol http2 --heartbeat-interval 10s run --token $TOKEN
+Restart=on-failure
+RestartSec=15
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+sudo systemctl daemon-reload
+sudo systemctl enable cloudflared
+sudo systemctl start cloudflared
+
+echo -e "============= Done! - by katorly =============\n\n"
