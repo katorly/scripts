56aa5d1e292a1aa6b57a308971cbd332b4ce2a8d
Katorly <i@katorly.com>
2024-11-23 11:19:05 +0000

    feat: now docker install script supports china
    * Checks whether the IP is in China
    * Ask whether to use a mirror site to install Docker CE when IP is in China

--- debian/docker/install.sh
+++ debian/docker/install.sh
@@ -3,16 +3,27 @@
 clear
 echo -e "====== Docker install script - by katorly ======="
 
+DOCKER_SOURCE=https://download.docker.com
+LOCAL_IP=$(curl -s ifconfig.me)
+IP_LOCATION=$(curl -s "https://ipapi.co/${LOCAL_IP}/country/")
+if [ "$IP_LOCATION" == "CN" ]; then
+    echo -e "Your IP may be located in China."
+    read -p "Would you like to install Docker CE from USTC mirror site? (y|n) " MIRROR_PLEASE
+    if [ "$MIRROR_PLEASE" == "y" ]; then
+        DOCKER_SOURCE=https://mirrors.ustc.edu.cn/docker-ce
+    fi
+fi
+
 echo -e "\n\nUninstalling old versions if any..."
 for pkg in docker.io docker-doc docker-compose podman-docker containerd runc; do sudo apt-get remove $pkg; done
 
 echo -e "\n\nInstalling packages..."
 sudo apt-get update -y && sudo apt-get install -y sudo ufw ca-certificates curl jq
 sudo install -m 0755 -d /etc/apt/keyrings
-sudo curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
+sudo curl -fsSL $DOCKER_SOURCE/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
 sudo chmod a+r /etc/apt/keyrings/docker.asc
 echo \
-    "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian \
+    "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] $DOCKER_SOURCE/linux/debian \
     $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
     sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update -y
